d96f9f6c4608fb8f310f29e4e86339b6d66ae1ff
wallebach <114591465+wallebach@users.noreply.github.com>
2023-12-14 22:18:53 +0100

    5. Random numbers - block.timestamp

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -13,26 +13,34 @@ contract Raffle {
     error Raffle__NotEnoughEthSent();
 
     uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
     address payable[] private s_players;
+    uint256 private s_lastTimestamp;
 
     /**
      * Events
      */
     event EnteredRaffle(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(uint256 entranceFee, uint256 interval) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
     }
 
     function enterRaffle() public payable {
         if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughEthSent();
         }
+        s_lastTimestamp = block.timestamp;
         s_players.push(payable(msg.sender));
         emit EnteredRaffle(msg.sender);
     }
 
-    function pickWinner() public {}
+    function pickWinner() public {
+        if ((block.timestamp - s_lastTimestamp) < i_interval) {
+            revert();
+        }
+    }
 
     /**
      * Getter functions
