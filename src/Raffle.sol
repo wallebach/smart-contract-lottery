ac03ecd85ddf8044e114e2f8234caa50971111ac
wallebach <114591465+wallebach@users.noreply.github.com>
2023-12-14 22:13:52 +0100

    4. Smart contract events

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -7,9 +7,18 @@ pragma solidity ^0.8.18;
  * @notice This contract is for creating a sample raffle
  */
 contract Raffle {
+    /**
+     * Errors
+     */
     error Raffle__NotEnoughEthSent();
 
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /**
+     * Events
+     */
+    event EnteredRaffle(address indexed player);
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
@@ -19,6 +28,8 @@ contract Raffle {
         if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughEthSent();
         }
+        s_players.push(payable(msg.sender));
+        emit EnteredRaffle(msg.sender);
     }
 
     function pickWinner() public {}
