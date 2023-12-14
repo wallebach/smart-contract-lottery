3c5450d6bd9deae8a00c3eb256af052eb42e8d2c
wallebach <114591465+wallebach@users.noreply.github.com>
2023-12-14 21:41:43 +0100

    1. Project setup

---
+++ src/Raffle.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+/**
+ * @title Sample Raffle Contract
+ * @author wallebach
+ * @notice This contract is for creating a sample raffle
+ */
+contract Raffle {
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {}
+
+    function pickWinner() public {}
+
+    /**
+     * Getter functions
+     */
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+}
