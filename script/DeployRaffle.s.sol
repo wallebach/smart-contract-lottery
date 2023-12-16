dbd82464e519fa67496c76c0e36e069d4461f411
wallebach <114591465+wallebach@users.noreply.github.com>
2023-12-16 19:45:04 +0100

    Lesson 18: Tests and deploy the lotterys smart contract pt.2

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -2,5 +2,31 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {Raffle} from "../src/Raffle.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
-contract DeployRaffle is Script {}
+contract DeployRaffle is Script {
+    function run() external returns (Raffle, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig();
+        (
+            uint256 entranceFee,
+            uint256 interval,
+            address vrfCoordintor,
+            bytes32 gasLane,
+            uint64 subscriptionId,
+            uint32 callbackGasLimit
+        ) = helperConfig.activeNetworkConfig();
+
+        vm.startBroadcast();
+        Raffle raffle = new Raffle(
+            entranceFee,
+            interval,
+            vrfCoordintor,
+            gasLane,
+            subscriptionId,
+            callbackGasLimit
+        );
+        vm.stopBroadcast();
+        return (raffle, helperConfig);
+    }
+}
--- src/Raffle.sol
+++ src/Raffle.sol
@@ -153,4 +153,8 @@ contract Raffle is VRFConsumerBaseV2 {
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
+
+    function getRaffleState() public view returns (RaffleState) {
+        return s_raffleState;
+    }
 }
